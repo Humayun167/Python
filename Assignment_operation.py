8ef02b14064fce3b2d77cebbdf2b670f1c698ce1
Md Humayun Rashid Khan <123394676+Humayun167@users.noreply.github.com>
2024-02-05 01:42:30 +0600

    latest

--- Assignment_operation.py
+++ Assignment_operation.py
@@ -1,10 +1,11 @@
 a = int(input('Enter the value of first variable'))
-b= int(input("Ebter value of second variable "))
+b = int(input("Ebter value of second variable "))
 
-print('a=b', a==b)
-print('a+=b', a+b)
-print('a-=b', a-b)
-print('a*=b',a*b)
-print('a%=b:', a%b)
-print('a**=b', a**b)
+print('a=b', a == b)
+print('a+=b', a + b)
+print('a-=b', a - b)
+print('a*=b', a * b)
+print('a%=b:', a % b)
+print('a**=b', a ** b)
 print('a//=b', a//b)
+ 
