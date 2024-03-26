9e90e52e07e94297871bb1bf2a3866536657d9d9
Md Humayun Rashid Khan <123394676+Humayun167@users.noreply.github.com>
2024-03-26 22:08:14 +0600

    latestT

---
+++ beecrawd1070.py
@@ -0,0 +1,8 @@
+x = int(input(" Enter the number: "))
+
+if x % 2 == 0:
+    x += 1
+
+for i in range(6):
+    print(x)
+    x += 2
--- prime_number.py
+++ prime_number.py
@@ -1,10 +1,42 @@
-lower = int(input(""))
-upper = int(input(""))
+# lower = int(input(""))
+# upper = int(input(""))
+#
+# for num in range (lower, upper + 1):
+#     if num > 1:
+#         for i in range(2, num):
+#             if num%i == 0:
+#                 break
+#             else:
+#                 print(num)
 
-for num in range (lower, upper + 1):
-    if num > 1:
-        for i in range(2, num):
-            if num%i == 0:
-                break
-            else:
-                print(num)
+# num = 97
+# for i in range(2, num):
+#     if num % i == 0:
+#         print("This is not prime")
+#         break
+# else:
+#     print("This is prime")
+import math
+
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+# Example usage:
+number = 97
+if is_prime(number):
+    print(number, "is a prime number")
+else:
+    print(number, "is not a prime number")
