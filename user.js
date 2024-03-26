36182aa31d757cf7712ebf385bc39783efffc86c
Md Humayun Rashid Khan <123394676+Humayun167@users.noreply.github.com>
2024-03-26 22:10:48 +0600

    Update user.js

--- user.js
+++ user.js
@@ -1,14 +1,1 @@
-const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('Enter your number: ', (a) => {
-  rl.question('Enter number-2: ', (b) => {
-    const sum = parseInt(a) + parseInt(b);
-    console.log('Sum:', sum);
-    rl.close();
-  });
-});
